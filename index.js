72904fa418306bbb54a8c1cf4d48522c79a334ac
Xuwei Li <dev@xuwei.li>
2021-09-06 07:03:53 +0000

    Add `login` command.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { program } = require('commander')
 
 require('./v4ex-api-authentication/register')
+require('./v4ex-api-authentication/login')
 
 
 program.parse()
--- v4ex-api-authentication/login.js
+++ v4ex-api-authentication/login.js
@@ -2,17 +2,17 @@ const { program } = require('commander')
 
 const { authentication } = require('v4ex-api-authentication')
 
-program.command('register')
+program.command('login')
        .argument('<username>', 'username to login')
-       .argument('[password]', '(optional) password paired with the username')
+       .argument('<password>', 'password paired with the username')
        .action((username, password) => {
          console.log('username:', username);
          console.log('password:', password);
-         authentication.register(username, password, function(err, authenticationDoc) {
+         authentication.login(username, password, function(err, tokenDoc) {
            if (err) {
              console.log(err)
            }
           
-           console.log(authenticationDoc)
+           console.log(tokenDoc)
          })
        })
